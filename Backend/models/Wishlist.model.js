5ca2a747a424d8b45f3fcb98e5a58faa86b3b050
23IT049 Kothiya Darshilkumar Jaysukhbhai <152584914+23IT049@users.noreply.github.com>
2025-03-20 23:05:37 +0530

    Delete Backend/models/Wishlist.model.js

--- Backend/models/Wishlist.model.js
+++
@@ -1,8 +0,0 @@
-// const mongoose = require('mongoose');
-
-// const wishlistSchema = new mongoose.Schema({
-//   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-//   PhilatelicItem: [{ type: mongoose.Schema.Types.ObjectId, ref: 'PhilatelicItem' }]
-// }, { timestamps: true });
-
-// module.exports = mongoose.model('Wishlist', wishlistSchema);
