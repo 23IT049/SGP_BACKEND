064b9093cf43a1aba254b2516c92902e16297bf3
23IT049 Kothiya Darshilkumar Jaysukhbhai <152584914+23IT049@users.noreply.github.com>
2025-03-20 20:43:27 +0530

    Update authenticateuser.js

--- Backend/middlewares/authenticateuser.js
+++ Backend/middlewares/authenticateuser.js
@@ -33,25 +33,3 @@ module.exports = async (req, res, next) => {
   }
 };
 
-// Old version
-// module.exports = async (req, res, next) => {
-//   const token = req.header('Authorization')?.replace('Bearer ', '');
-
-//   if (!token) {
-//     return res.status(401).json({ message: 'No token provided' });
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     const user = await User.findById(decoded.id);
-
-//     if (!user) {
-//       return res.status(404).json({ message: 'User not found' });
-//     }
-
-//     req.user = user; // Set user for next middleware
-//     next();
-//   } catch (error) {
-//     res.status(401).json({ message: 'Invalid token' });
-//   }
-// };
